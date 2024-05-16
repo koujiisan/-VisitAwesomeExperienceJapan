0fc9c31e0f0d9e387a23dcae96f4f658b3ba4c24
koujiisan <ftltf.gircc.lmni.smai@gmail.com>
2024-05-17 07:42:43 +0900

    日本語化対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    extracted_locale = extract_locale_from_accept_language_header
+    Rails.logger.debug "Extracted Locale: #{extracted_locale}"
+    I18n.locale = extracted_locale || I18n.default_locale
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+  end
 end
